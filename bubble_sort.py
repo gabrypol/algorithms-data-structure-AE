b6fee9d823ed6c81bf0bfd3ad9d17a9957549759
Gabriele Polenta <gabrielepolenta@gmail.com>
2020-06-07 23:28:05 +0200

    bubble sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -10,13 +10,14 @@ Sample Output
 
 def bubble_sort(input_list):
     if len(input_list) == 0 or len(input_list) == 1:
-      return input_list
+        return input_list
 
     while True:
         sorted_pairs = 0
         for i, num in enumerate(input_list[1:], 1):
             if input_list[i] < input_list[i - 1]:
-                input_list[i], input_list[i - 1] = input_list[i - 1], input_list[i]
+                input_list[i], input_list[i -
+                                          1] = input_list[i - 1], input_list[i]
                 sorted_pairs += 1
         if sorted_pairs == 0:
             return input_list
