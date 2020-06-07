6dc3970b4ca512ccc6120b113d686ce1ef782d0a
Gabriele Polenta <g.polenta@orderfox.com>
2020-06-07 11:32:46 +0200

    two number sum

--- readme.md
+++ readme.md
@@ -1,9 +1,9 @@
 Here my solutions to the coding challenges on AlgoExpert.io
 
-## Recursion
-
-- [nth Fibonacci](./nth_fibonacci.py) (Easy)
-
 ## Arrays
 
 - [Two number sum](./two_number_sum.py) (Easy)
+
+## Recursion
+
+- [nth Fibonacci](./nth_fibonacci.py) (Easy)
--- two_number_sum.py
+++ two_number_sum.py
@@ -13,8 +13,38 @@ Sample Output
 '''
 
 '''
-This can be solved with a brute force approach. In that case the time complexity would be O(n^2) and the space complexity O(1).
-The solution shown below is much faster, although it requires O(n) space.
+SOLUTION 1:
+    This can be solved with a brute force approach. In that case the time complexity would be O(n^2) and the space complexity O(1).
+    The solution shown below is much faster, although it requires O(n) space.
+        Time: O(n^2)
+        Space: O(1)
+
+SOLUTION 2:
+    A second solution is to sort the input_list first and then use a left_pointer and right_pointer. If the current sum is greater than target_sum, then move the right pointer by one to the left. If the current sum is less than target_sum, then move the left pointer by one to the right. Else, if the current sum is equal to target_sum, then return the items of the list at left_pointer and right_pointer.
+    The time complexity is nlogn, because good sorting algorithms sort in nlogn time. Space complexity is constant O(1).
+        Time: O(nlogn)
+        Space: O(1)
+
+
+def twoNumberSum(input_list, target_sum):
+    input_list.sort()
+
+    left_pointer = 0
+    right_pointer = len(input_list) - 1
+    while (left_pointer < right_pointer):
+        if input_list[left_pointer] + input_list[right_pointer] < target_sum:
+            left_pointer += 1
+        elif input_list[left_pointer] + input_list[right_pointer] > target_sum:
+            right_pointer -= 1
+        else:
+            return [input_list[left_pointer], input_list[right_pointer]]
+    return []
+
+
+SOLUTION 3 (see below):
+    We use dynamic programming and store the visited items in a dictionary. This is the fastest solution, although it needs linear space.
+        Time: O(n)
+        Space: O(n)
 '''
 
 
@@ -25,12 +55,6 @@ def twoNumberSum(input_list, target_sum):
             return[num, target_sum - num]
         cache[num] = i
     return []
-
-
-'''
-Time: O(n)
-Space: O(n)
-'''
 
 
 my_list = [3, 5, -4, 8, 11, 1, -1, 6]
