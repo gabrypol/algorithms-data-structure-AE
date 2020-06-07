e3a8c09b405c7ca41f00c49fd719d9a4dab24158
Gabriele Polenta <gabrielepolenta@gmail.com>
2020-06-07 23:27:37 +0200

    insertion sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -9,12 +9,12 @@ Sample Output
 
 
 def insertion_sort(input_list):
-  for i in range(1, len(input_list)):
-    for j in range(i - 1, -1, -1):
-      if input_list[j + 1] < input_list[j]:
-        input_list[j + 1], input_list[j] = input_list[j], input_list[j + 1]
+    for i in range(1, len(input_list)):
+        for j in range(i - 1, -1, -1):
+            if input_list[j + 1] < input_list[j]:
+                input_list[j + 1], input_list[j] = input_list[j], input_list[j + 1]
 
-  return input_list
+    return input_list
 
 
 my_list = [8, 5, 2, 9, 5, 6, 3]
@@ -24,4 +24,3 @@ print(insertion_sort(my_list))
 Time: O(n^2)
 Space: O(1)
 '''
-
