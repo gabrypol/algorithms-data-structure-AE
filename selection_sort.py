3a673e67fe3d4d9bf741a796dc069aeb078af464
Gabriele Polenta <gabrielepolenta@gmail.com>
2020-06-13 09:15:35 +0200

    selection sort

---
+++ selection_sort.py
@@ -0,0 +1,30 @@
+'''
+Write a function that takes in an array of integers and returns a sorted version of that array. Use the Selection Sort algorithm to sort the array.
+
+If you're unfamiliar with Selection Sort, we recommend watching the Conceptual Overview section of this question's video explanation before starting to code.
+
+Sample Input
+array = [8, 5, 2, 9, 5, 6, 3]
+Sample Output
+[2, 3, 5, 5, 6, 8, 9]
+'''
+
+def selection_sort(input_list):
+    for i, num_one in enumerate(input_list[:len(input_list) - 1]):
+        min_idx = i
+        for j, num_two in enumerate(input_list[i + 1:], i + 1):
+            if num_two < input_list[min_idx]:
+                min_idx = j
+        input_list[i], input_list[min_idx] = input_list[min_idx], input_list[i]
+    return input_list
+
+
+my_list = [8, 5, 2, 9, 5, 6, 3]
+print(selection_sort(my_list))
+
+
+'''
+Time: O(n^2)
+Space: O(1)
+'''
+
