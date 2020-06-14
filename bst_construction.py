8d55e8e0ecb043fa8f2ba04a1e9b8effedc15332
Gabriele Polenta <gabrielepolenta@gmail.com>
2020-06-14 11:37:47 +0200

    bst construction

--- bst_construction.py
+++ bst_construction.py
@@ -38,6 +38,93 @@ remove(10):   12
 contains(15): true
 '''
 
+# RECURSIVE SOLUTION:
+# class BST:
+#     def __init__(self, value):
+#         self.value = value
+#         self.left = None
+#         self.right = None
+
+
+#     def insert(self, value):
+#         if value < self.value:
+#             if self.left is None:
+#                 self.left = BST(value)
+#             else:
+#                 self.left.insert(value)
+#         else:
+#             if self.right is None:
+#                 self.right = BST(value)
+#             else:
+#                 self.right.insert(value)
+#         return self
+#     '''
+#     Time: Average (balanced tree) --> O(log(n)) /---/ Worst case (unbalanced tree) --> O(n)
+#     Space: Average (balanced tree) --> O(log(n)) /---/ Worst case (unbalanced tree) --> O(n)
+#     '''
+
+
+#     def contains(self, value):
+#         if value < self.value:
+#             if self.left is None:
+#                 return False
+#             else:
+#                 return self.left.contains(value)
+#         elif value > self.value:
+#             if self.right is None:
+#                 return False
+#             else:
+#                 return self.right.contains(value)
+#         else:
+#             return True
+#     '''
+#     Time: Average (balanced tree) --> O(log(n)) /---/ Worst case (unbalanced tree) --> O(n)
+#     Space: Average (balanced tree) --> O(log(n)) /---/ Worst case (unbalanced tree) --> O(n)
+#     '''
+
+
+#     def remove(self, value, parent=None):
+#         if value < self.value:
+#             if self.left is not None:
+#                 self.left.remove(value, self)
+#         elif value > self.value:
+#             if self.right is not None:
+#                 self.right.remove(value, self)
+#         else:
+#             if self.left is not None and self.right is not None:
+#                 self.value = self.right.get_min_value()
+#                 self.right.remove(self.value, self)
+#             elif parent is None:
+#                 if self.left is not None:
+#                     self.value = self.left.value
+#                     self.right = self.left.right
+#                     self.left = self.left.left
+#                 elif self.right is not None:
+#                     self.value = self.right.value
+#                     self.left = self.right.left
+#                     self.right = self.right.right
+#                 else:
+#                     pass
+#             elif parent.left == self:
+#                 parent.left = self.left if self.left is not None else self.right
+#             elif parent.right == self:
+#                 parent.right = self.left if self.left is not None else self.right
+#         return self
+#     '''
+#     Time: Average (balanced tree) --> O(log(n)) /---/ Worst case (unbalanced tree) --> O(n)
+#     Space: Average (balanced tree) --> O(log(n)) /---/ Worst case (unbalanced tree) --> O(n)
+#     '''
+
+#     def get_min_value(self):
+# 	    if self.left is None:
+# 		    return self.value
+# 	    else:
+# 		    return self.left.get_min_value()
+
+
+
+
+# ITERATIVE SOLUTION:
 class BST:
     def __init__(self, value):
         self.value = value
@@ -46,80 +133,80 @@ class BST:
 
 
     def insert(self, value):
-        if value < self.value:
-            if self.left is None:
-                self.left = BST(value)
+        while True:
+            if value < self.value:
+                if self.left is None:
+                    self.left = BST(value)
+                    break
+                else:
+                    self = self.left
             else:
-                self.left.insert(value)
-        else:
-            if self.right is None:
-                self.right = BST(value)
-            else:
-                self.right.insert(value)
+                if self.right is None:
+                    self.right = BST(value)
+                    break
+                else:
+                    self = self.right
         return self
     '''
     Time: Average (balanced tree) --> O(log(n)) /---/ Worst case (unbalanced tree) --> O(n)
-    Space: Average (balanced tree) --> O(log(n)) /---/ Worst case (unbalanced tree) --> O(n)
+    Space: O(1)
     '''
 
 
     def contains(self, value):
-        if value < self.value:
-            if self.left is None:
-                return False
+        while self is not None:
+            if value < self.value:
+                self = self.left
+            elif value > self.value:
+                self = self.right
             else:
-                return self.left.contains(value)
-        elif value > self.value:
-            if self.right is None:
-                return False
-            else:
-                return self.right.contains(value)
-        else:
-            return True
+                return True
+        return False
     '''
     Time: Average (balanced tree) --> O(log(n)) /---/ Worst case (unbalanced tree) --> O(n)
-    Space: Average (balanced tree) --> O(log(n)) /---/ Worst case (unbalanced tree) --> O(n)
+    Space: O(1)
     '''
 
 
     def remove(self, value, parent=None):
-        if value < self.value:
-            if self.left is not None:
-                self.left.remove(value, self)
-        elif value > self.value:
-            if self.right is not None:
-                self.right.remove(value, self)
-        else:
-            if self.left is not None and self.right is not None:
-                self.value = self.right.get_min_value()
-                self.right.remove(self.value, self)
-            elif parent is None:
-                if self.left is not None:
-                    self.value = self.left.value
-                    self.right = self.left.right
-                    self.left = self.left.left
-                elif self.right is not None:
-                    self.value = self.right.value
-                    self.left = self.right.left
-                    self.right = self.right.right
-                else:
-                    pass
-            elif parent.left == self:
-                parent.left = self.left if self.left is not None else self.right
-            elif parent.right == self:
-                parent.right = self.left if self.left is not None else self.right
+        while self is not None:
+            if value < self.value:
+                parent = self
+                self = self.left
+            elif value > self.value:
+                parent = self
+                self = self.right
+            else:
+                if self.left is not None and self.right is not None:
+                    self.value = self.right.get_min_value()
+                    self.right.remove(self.value, self) ###
+                elif parent is None:
+                    if self.left is not None:
+                        self.value = self.left.value
+                        self.left = self.left.left
+                        self.right = self.left.right
+                    elif self.right is not None:
+                        self.value = self.right.value
+                        self.left = self.right.left
+                        self.right = self.right.right
+                    else:
+                        pass
+                elif parent.left == self:
+                    parent.left = self.left if self.left is not None else self.right
+                elif parent.right == self:
+                    parent.right = self.left if self.left is not None else self.right
+                break
         return self
     '''
     Time: Average (balanced tree) --> O(log(n)) /---/ Worst case (unbalanced tree) --> O(n)
-    Space: Average (balanced tree) --> O(log(n)) /---/ Worst case (unbalanced tree) --> O(n)
+    Space: O(1)
     '''
 
-    def get_min_value(self):
-	    if self.left is None:
-		    return self.value
-	    else:
-		    return self.left.get_min_value()
 
+    def get_min_value(self):
+        while self.left is not None:
+            self = self.left
+        return self.value
 
 #          10
 #        /     \
